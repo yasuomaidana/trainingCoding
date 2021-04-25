cface1e5f0e9c5882e70fad07a045759ea3ebda7
yasuomaidana <yasuomaidana@gmail.com>
2021-04-25 11:00:48 -0500

    basic zip options

--- MahoutMovieRecommender/src/main/java/com/MahoutMovieRecommender/MahoutMovieRecommender/App.java
+++ MahoutMovieRecommender/src/main/java/com/MahoutMovieRecommender/MahoutMovieRecommender/App.java
@@ -1,10 +1,12 @@
 package com.MahoutMovieRecommender.MahoutMovieRecommender;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.IOException;
-
-import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
-import org.apache.mahout.cf.taste.model.DataModel;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 /**
  * Hello world!
@@ -14,7 +16,33 @@ public class App
 {
     public static void main( String[] args ) throws IOException
     {
-    	//DataModel model = new FileDataModel(new File("/Users/nsl-intern/Downloads/movies.txt"));
+    	//Get the current name
+    	String baseName = System.getProperty("user.dir")+"/";
+    	
+    	//Read the data contained in data.zip
+    	ZipFile zipFile = new ZipFile(baseName+"data/data.zip");
+    	Enumeration<? extends ZipEntry> entries = zipFile.entries();
+    	while (entries.hasMoreElements()) {
+            ZipEntry entry = entries.nextElement();
+            String name = entry.getName();
+            long compressedSize = entry.getCompressedSize();
+            long normalSize = entry.getSize();
+            String type = entry.isDirectory() ? "DIR" : "FILE";
+
+            System.out.println(name);
+            System.out.format("\t %s - %d - %d\n", type, compressedSize, normalSize);
+            
+            InputStream input = zipFile.getInputStream(entry);
+            BufferedReader br = new BufferedReader(new InputStreamReader(input, "UTF-8"));
+            while(br.readLine()!=null){
+            	System.out.println(br.readLine());
+            	break;
+            }
+        }
+
+        zipFile.close();
+    	//DataModel model = new FileDataModel(new File("/Users/nsl-intern/Documents/Academy Code/MahoutMovieRecommender/data/data.csv"));
+        System.out.println("It worked");
     }
 }
 class Data{
