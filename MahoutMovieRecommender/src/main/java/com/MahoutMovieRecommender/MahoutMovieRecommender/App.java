6c0724335492e6624f2054231bc1779f76bbac5f
yasuomaidana <yasuomaidana@gmail.com>
2021-04-25 11:32:44 -0500

    extraction done

--- MahoutMovieRecommender/src/main/java/com/MahoutMovieRecommender/MahoutMovieRecommender/App.java
+++ MahoutMovieRecommender/src/main/java/com/MahoutMovieRecommender/MahoutMovieRecommender/App.java
@@ -1,12 +1,17 @@
 package com.MahoutMovieRecommender.MahoutMovieRecommender;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
 
 /**
  * Hello world!
@@ -16,35 +21,52 @@ public class App
 {
     public static void main( String[] args ) throws IOException
     {
-    	//Get the current name
-    	String baseName = System.getProperty("user.dir")+"/";
-    	
-    	//Read the data contained in data.zip
-    	ZipFile zipFile = new ZipFile(baseName+"data/data.zip");
-    	Enumeration<? extends ZipEntry> entries = zipFile.entries();
-    	while (entries.hasMoreElements()) {
-            ZipEntry entry = entries.nextElement();
-            String name = entry.getName();
-            long compressedSize = entry.getCompressedSize();
-            long normalSize = entry.getSize();
-            String type = entry.isDirectory() ? "DIR" : "FILE";
-
-            System.out.println(name);
-            System.out.format("\t %s - %d - %d\n", type, compressedSize, normalSize);
-            
-            InputStream input = zipFile.getInputStream(entry);
-            BufferedReader br = new BufferedReader(new InputStreamReader(input, "UTF-8"));
-            while(br.readLine()!=null){
-            	System.out.println(br.readLine());
-            	break;
-            }
-        }
-
-        zipFile.close();
-    	//DataModel model = new FileDataModel(new File("/Users/nsl-intern/Documents/Academy Code/MahoutMovieRecommender/data/data.csv"));
-        System.out.println("It worked");
+    	Data data = new Data();
     }
 }
-class Data{
 
+// This class contains all the functions and parameters related with data
+class Data{
+	//Directory's path
+	String pathDir;
+	int BUFFER_SIZE = 4096;
+	// Total information
+	int TotalReviews;
+	int TotalProducts;
+	int TotalUsers;
+	
+	//Constructor
+	public Data() throws IOException{
+		//Get the current name
+    	pathDir = System.getProperty("user.dir")+"/";
+		getDat();
+	}
+	
+	
+	//Read the data.zip file and unzip it
+	public void getDat() throws IOException {
+		//Read the data contained in data.zip
+    	ZipInputStream zipIn = new ZipInputStream(new FileInputStream(pathDir+"data/data.zip"));
+        ZipEntry entry = zipIn.getNextEntry();
+        // iterates over entries in the zip file
+        while (entry != null) {
+            String filePath = pathDir +"data/data.csv";
+                extractFile(zipIn, filePath);
+            zipIn.closeEntry();
+            entry = zipIn.getNextEntry();
+        }
+        zipIn.close();
+        System.out.println("Data.zip decompressed");
+        
+	}
+	//Extracts the file
+	private void extractFile(ZipInputStream zipIn, String filePath) throws IOException {
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
+        byte[] bytesIn = new byte[BUFFER_SIZE];
+        int read = 0;
+        while ((read = zipIn.read(bytesIn)) != -1) {
+            bos.write(bytesIn, 0, read);
+        }
+        bos.close();
+    }
 }
