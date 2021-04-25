0d1dbfdf6869429541d2f521a45041901f16ba52
yasuomaidana <yasuomaidana@gmail.com>
2021-04-25 13:37:06 -0500

    before tests

--- MahoutMovieRecommender/src/main/java/com/MahoutMovieRecommender/MahoutMovieRecommender/App.java
+++ MahoutMovieRecommender/src/main/java/com/MahoutMovieRecommender/MahoutMovieRecommender/App.java
@@ -15,18 +15,18 @@ public class App
     {
     	MovieRecommender recommender = new MovieRecommender();
     	
-    	assertEquals(7911684, recommender.getTotalReviews());
-        assertEquals(253059, recommender.getTotalProducts());
-        assertEquals(889176, recommender.getTotalUsers());
+    	//assertEquals(7911684, recommender.getTotalReviews());
+        //assertEquals(253059, recommender.getTotalProducts());
+        //assertEquals(889176, recommender.getTotalUsers());
 
         List<String> recommendations = recommender.getRecommendationsForUser("A141HP4LYPWMSR");
-        /*for (String i:recommendations) {
+        for (String i:recommendations) {
         	System.out.println(i);
-        }*/
+        }
         
-        assertThat(recommendations, hasItem("B0002O7Y8U"));
-        assertThat(recommendations, hasItem("B00004CQTF"));
-        assertThat(recommendations, hasItem("B000063W82"));
+        //assertThat(recommendations, hasItem("B0002O7Y8U"));
+        //assertThat(recommendations, hasItem("B00004CQTF"));
+        //assertThat(recommendations, hasItem("B000063W82"));
     }
 }
 
