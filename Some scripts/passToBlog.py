531e5cbfc11656e5195a8bc3ad0cbcc02c488d16
yasuomaidana <yasuomaidana@gmail.com>
2021-06-08 18:04:39 -0500

    Some scripts used for esay journal

---
+++ Some scripts/passToBlog.py
@@ -0,0 +1,10 @@
+paragraph = """<mat-expansion-panel hideToggle class="ExpansionPanel">
+    <mat-expansion-panel-header class="matPanelTitle">
+    <mat-panel-title>
+        <p> ?T </p>
+        </mat-panel-title>
+    </mat-expansion-panel-header>
+    ?C
+</mat-expansion-panel>"""
+
+print(paragraph)
