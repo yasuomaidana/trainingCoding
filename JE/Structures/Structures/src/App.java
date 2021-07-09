9dce8c470200fe4dc83291e25bb18b00fe84664a
yasuomaidana <yasuomaidana@gmail.com>
2021-07-09 18:28:18 -0500

    Snake Game workshop

--- JE/Structures/Structures/src/App.java
+++ JE/Structures/Structures/src/App.java
@@ -1,13 +1,7 @@
+import tests.test;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        tests();
-    }
-    public static void tests(){
-        System.out.println("Portion of code used to test usage of stack");
-        new Balanced_Stack();
-        System.out.println();
-        System.out.println("Portion of code used to test usage of queue");
-        new Palindrome_Queue();
+        test.main();
     }
 }
