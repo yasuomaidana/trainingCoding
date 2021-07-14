05295da948164bbaa5c2869f1d9356809e87cc1e
yasuomaidana <yasuomaidana@gmail.com>
2021-07-13 19:18:53 -0500

    converted into console callable program

--- Some scripts/WtH/main.py
+++ Some scripts/WtH/main.py
@@ -4,8 +4,17 @@ def writeFile(filename,data):
     processFile = open(filename, 'w')
     processFile.writelines(data)
     processFile.close()
-
-filename="Week 15"
+if __name__ == "__main__":
+    import sys
+    l=len(sys.argv)    
+    if(l==1):
+        filename=input("Html filename located at downloads : \n: ")
+    elif(l==2):
+        filename=(str(sys.argv[1]))
+    else:
+        filename=(str(sys.argv[1]))
+        for word in range(2,l):
+            filename+=" "+(str(sys.argv[word]))
 parser = html_processor()
 file = open("/Users/nsl-intern/Downloads/{}.html".format(filename))
 proccessedFile = parser.replace(file)
