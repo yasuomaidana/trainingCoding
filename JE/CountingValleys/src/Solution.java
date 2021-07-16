6ac5f45d6cb4d4cbc05a8b341448e2882f0a1d8e
yasuomaidana <yasuomaidana@gmail.com>
2021-07-16 10:59:09 -0500

    Counting Valleys java excersice done

---
+++ JE/CountingValleys/src/Solution.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Result {
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, String path) {
+        boolean newValley = true;
+        int level=0;
+        int valleys=0;
+        for(Character letter:path.toCharArray()){
+            if(letter=='D') level--;
+            else level++;
+            if(newValley&&level<0){
+                valleys++;
+                newValley = false;
+            }
+            if(!newValley&&level==0){
+                newValley = true;
+            }
+        }
+        return valleys;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("result.txt"));
+
+        int steps = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String path = bufferedReader.readLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
