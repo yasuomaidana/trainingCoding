4503ebe874635a5c0872734745818e6d0d762b4b
yasuomaidana <yasuomaidana@gmail.com>
2021-07-18 12:06:02 -0500

    unused import removed

--- JE/LeftRotation/src/Solution.java
+++ JE/LeftRotation/src/Solution.java
@@ -6,7 +6,6 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
