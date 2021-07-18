67c9d711ce613322b59be05268923d24994e40d1
yasuomaidana <yasuomaidana@gmail.com>
2021-07-18 18:20:53 -0500

    30 days challenge started

---
+++ JE/30Days/defVariables/src/Solution.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+        /* Declare second integer, double, and String variables. */
+        int i2 = scan.nextInt();
+        double d2 = scan.nextDouble();
+        String s2 = scan.nextLine()+scan.nextLine();
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i+i2);
+        /* Print the sum of the double variables on a new line. */
+		System.out.println(d+d2);
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+        System.out.println(s+s2);
+        scan.close();
+    }
+}
