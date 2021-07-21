1825c82520f3e4b4b5a0c8ea8b1c382f5d13231d
yasuomaidana <yasuomaidana@gmail.com>
2021-07-21 18:07:00 -0500

    some functions added

--- JE/SwingEclipseJava/src/Frame1.java
+++ JE/SwingEclipseJava/src/Frame1.java
@@ -7,11 +7,16 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.JTextPane;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Frame1 {
 
 	private JFrame frame;
-
+	private JLabel LabelToChange;
+	private JButton RestoreLabel;
+	private JButton FirstButton;
+	private boolean seeFirstButton;
 	/**
 	 * Launch the application.
 	 */
@@ -38,24 +43,34 @@ public class Frame1 {
 	/**
 	 * Initialize the contents of the frame.
 	 */
+	private void toggleVisibility() {
+		RestoreLabel.setVisible(!seeFirstButton);
+		FirstButton.setVisible(seeFirstButton);
+	}
 	private void initialize() {
+		
+		seeFirstButton = true;
 		frame = new JFrame();
 		frame.setBounds(100, 100, 485, 373);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("Yasuo Button");
-		btnNewButton.addActionListener(new ActionListener() {
+		FirstButton = new JButton("Yasuo Button");
+		FirstButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "Holi Yasuo");
+				JOptionPane.showMessageDialog(null, "Changing label");
+				LabelToChange.setText("Label Changed");
+				seeFirstButton = false;
+				toggleVisibility();
 			}
 		});
-		btnNewButton.setBounds(185, 237, 117, 29);
-		frame.getContentPane().add(btnNewButton);
+		FirstButton.setBounds(185, 237, 117, 29);
+		frame.getContentPane().add(FirstButton);
 		
-		JLabel lblNewLabel = new JLabel("Holi\n\nLos labels NO pueden tener multiples lineas");
-		lblNewLabel.setBounds(80, 49, 342, 64);
-		frame.getContentPane().add(lblNewLabel);
+		LabelToChange = new JLabel("Holi\n\nLos labels NO pueden tener multiples lineas");
+		LabelToChange.setLabelFor(frame);
+		LabelToChange.setBounds(80, 49, 342, 64);
+		frame.getContentPane().add(LabelToChange);
 		
 		JTextPane txtpnHola = new JTextPane();
 		txtpnHola.setEnabled(false);
@@ -63,5 +78,18 @@ public class Frame1 {
 		txtpnHola.setText("Hola\n\nEste es un ejemplo\n\nDe Multiples lineas\n");
 		txtpnHola.setBounds(80, 125, 205, 88);
 		frame.getContentPane().add(txtpnHola);
+		
+		RestoreLabel = new JButton("Restore label text");
+		RestoreLabel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				LabelToChange.setText("Holi\n\nLos labels NO pueden tener multiples lineas");
+				seeFirstButton=true;
+				toggleVisibility();
+			}
+		});
+		RestoreLabel.setBounds(170, 268, 144, 29);
+		frame.getContentPane().add(RestoreLabel);
+		RestoreLabel.setVisible(!seeFirstButton);
 	}
 }
