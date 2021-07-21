98c7d9060c2d0427cbd696bb518143f70b94d3a6
yasuomaidana <yasuomaidana@gmail.com>
2021-07-21 10:02:47 -0500

    hashmap approach

--- JE/ArrayManipulation/src/Solution.java
+++ JE/ArrayManipulation/src/Solution.java
@@ -4,7 +4,6 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.PortUnreachableException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -28,7 +27,10 @@ class Result {
     static int ll =(int) 1e10;
     //Upper limit
     static int ul=0;
+    //Limits hashmap
     static HashMap<List<Integer>,Long> sectionsSum;
+    static boolean notUsed = true;
+
     public static List<Integer> limits(int a,int b){
         List<Integer> limitList = new ArrayList<>();
         limitList.add(a);
@@ -51,22 +53,42 @@ class Result {
         int s_ll = key.get(0);
         int s_ul = key.get(1);
         long s_s = sectionsSum.get(key);
+        if(s_ll>qul) return sectionsSum;
         sectionsSum.remove(key);
         if(qul+1<=s_ul){
             fillMap(sectionsSum, qul+1, s_ul, s_s);
+            notUsed = false;
         }
         if(qll<=s_ll){
             fillMap(sectionsSum, qll, s_ll-1, qs);
             fillMap(sectionsSum, s_ll, qul, qs+s_s);
             max = check_max(qs+s_s);
+            notUsed = false;
         }
         if(qll>s_ll){
             fillMap(sectionsSum, s_ll, qll-1, s_s);
             if(s_ul>qll) s_ul=qul;
             fillMap(sectionsSum, qll, s_ul, s_s);
             fillFromLeft(sectionsSum, qll, qul, qs, limits(qll, s_ul));
+            notUsed = false;
         }
         return sectionsSum;
+    }
+    public static boolean inLimits(int qul, int qll,int s){
+        //If range outside of lists
+        if(qul<ll){
+            ll=qll;
+            fillMap(sectionsSum, qll, qul, s);
+            max = check_max(s);
+            return false;
+        }
+        if(ul<qll){
+            ul=qul;
+            fillMap(sectionsSum, qll, qul, s);
+            max = check_max(s);
+            return false;
+        }
+        return true;
     }
     public static List<Integer> edgeLimits(List<Integer> query){
         List<Integer> edgLimits = new ArrayList<>();
@@ -79,55 +101,64 @@ class Result {
         // Summand
         int s = query.get(2);
         //Initialize hashmap
+        int continue_=0;
         if(sectionsSum.isEmpty()){
             ll=qll;
             ul=qul;
             fillMap(sectionsSum, qll, qul, s);
             max = s;
-            continue;
+            continue_=-1;
         }
-        //If range outside of lists
-        if(qul<ll){
-            ll=qll;
-            fillMap(sectionsSum, qll, qul, s);
-            max = check_max(s);
-            continue;
-        }
-        if(ul<qll){
-            ul=qul;
-            fillMap(sectionsSum, qll, qul, s);
-            max = check_max(s);
-            continue;
-        }
+        if(!inLimits(qul, qll, s)) continue_=-1;
         if(qll<ll) ll=qll;
         if(qul>ul) ul = qul;
-        
+        edgLimits.add(continue_);
         return edgLimits;
+    }
+    public static List<List<Integer>> searchInSections(List<Integer> limits,int s){
+        int qll = limits.get(0);
+        int qul = limits.get(1);
+        List<List<Integer>> sectionToCheck = new ArrayList<>();
+        if(!inLimits(qul, qll, s)){
+            return sectionToCheck;
+        }
+        for(Entry<List<Integer>, Long> key:sectionsSum.entrySet()){
+            int s_ll = key.getKey().get(0);
+            int s_ul = key.getKey().get(1);
+            if(qul<=s_ul){
+                fillFromLeft(sectionsSum, qll, qul, s, key.getKey());
+            }
+            else if(qll>s_ll && qll<=s_ul){
+                fillFromLeft(sectionsSum, qll, s_ul, s, key.getKey());
+                notUsed = true;
+                qll = s_ul+1;
+            }
+            else if(qll<s_ll && qul>s_ul){
+                fillFromLeft(sectionsSum, s_ll, s_ul, s, key.getKey());
+                sectionToCheck.add(limits(qll, s_ll-1));
+                sectionToCheck.add(limits(s_ul+1, qul));
+                return sectionToCheck;
+            }
+        }
+        if(notUsed) fillMap(sectionsSum, qll, qul, s);
+        return sectionToCheck;
     }
     public static long arrayManipulation(int n, List<List<Integer>> queries) {
         sectionsSum =  new HashMap<>();
         Stack<List<Integer>> toSearch = new Stack<>();
-
-        for(List<Integer> query:queries){
-            
-            boolean notUsed = true;
-            for(Entry<List<Integer>, Long> key:sectionsSum.entrySet()){
-                int s_ll = key.getKey().get(0);
-                int s_ul = key.getKey().get(1);
-                if(qul<=s_ul){
-                    fillFromLeft(sectionsSum, qll, qul, s, key.getKey());
-                    notUsed = false;
-                }
-                else if(qll>s_ll && qll<=s_ul){
-                    fillFromLeft(sectionsSum, qll, s_ul, s, key.getKey());
-                    notUsed = true;
-                    qll = s_ul+1;
-                }
-                else if(qll<s_ll && qul>s_ul){
-                    fillFromLeft(sectionsSum, qll, s_ul, s, key.getKey());
-                }
+        //HashMap<List<Integer>, Long> lookSections = sectionsSum;
+        edgeLimits(queries.get(0));
+        for(int i =1;i<queries.size();i++){
+            List<Integer> query = queries.get(i);
+            int s = query.get(2);
+            query.remove(2);
+            toSearch.add(query);
+            //lookSections = sectionsSum;
+            while(!toSearch.empty()){
+                List<List<Integer>> newSections = searchInSections(toSearch.pop(), s);
+                newSections.forEach((section)->toSearch.add(section));
+                //lookSections = sectionsSum;
             }
-            if(notUsed) fillMap(sectionsSum, qll, qul, s);
         }
         return max;
     }
