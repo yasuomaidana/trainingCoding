6ca9f26b304a14d7df8b5a230b2e6345199c08bf
yasuomaidana <yasuomaidana@gmail.com>
2021-07-21 16:47:10 -0500

    using inheritance

--- JE/SwingTutorial/src/App.java
+++ JE/SwingTutorial/src/App.java
@@ -1,17 +1,15 @@
 import javax.swing.*;
-public class App {  
+public class App extends JFrame{//inheriting JFrame  
     JFrame f;  
     App(){  
-        f=new JFrame();//creating instance of JFrame  
-        JButton b=new JButton("click");//creating instance of JButton  
+        JButton b=new JButton("click");//create button  
         b.setBounds(130,100,100, 40);  
-        
-        f.add(b);//adding button in JFrame  
-        f.setSize(400,500);//400 width and 500 height  
-        f.setLayout(null);//using no layout managers  
-        f.setVisible(true);//making the frame visible  
+        add(b);//adding button on frame  
+        setSize(400,500);  
+        setLayout(null);  
+        setVisible(true);  
     }  
     public static void main(String[] args) {  
         new App();  
-    }      
+    }
 }  
