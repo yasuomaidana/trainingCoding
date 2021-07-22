39a14a340cd33d6b0709ce9f8c8c5063c9c4ec25
yasuomaidana <yasuomaidana@gmail.com>
2021-07-21 23:07:52 -0500

    Loops excersice done

--- JE/30Days/Loops/src/Solution.java
+++ JE/30Days/Loops/src/Solution.java
@@ -1,5 +1,15 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        for(int i=1;i<=10;i++){
+            System.out.println(n+" x "+i+" = "+i*n);
+        }
+        bufferedReader.close();
     }
 }
