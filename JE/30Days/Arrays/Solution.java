bac0bdefc426d65fd8d9a498fce0a45d7abc2c1e
yasuomaidana <yasuomaidana@gmail.com>
2021-07-23 15:32:40 -0500

    excersice done

---
+++ JE/30Days/Arrays/Solution.java
@@ -0,0 +1,25 @@
+import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+        for(int i=n-1;i>-1;i--){
+            System.out.print(arr.get(i)+" ");
+        }
+        bufferedReader.close();
+    }
+}
