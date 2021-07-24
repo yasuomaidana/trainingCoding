063dd47301f72df1c216a8b7974b41a16c50c5d9
yasuomaidana <yasuomaidana@gmail.com>
2021-07-23 22:33:50 -0500

    done

---
+++ PE/HashTables/RansomNote/ransomnote.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'checkMagazine' function below.
+#
+# The function accepts following parameters:
+#  1. STRING_ARRAY magazine
+#  2. STRING_ARRAY note
+#
+
+def checkMagazine(magazine, note):
+    words = {}
+    for word in magazine:
+        words[word] = words.get(word,-1)+1
+    for word in note:
+        if(words.get(word,-1)<0):
+            print("No")
+            return
+        words[word]-=1
+    print("Yes")
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    m = int(first_multiple_input[0])
+
+    n = int(first_multiple_input[1])
+
+    magazine = input().rstrip().split()
+
+    note = input().rstrip().split()
+
+    checkMagazine(magazine, note)
