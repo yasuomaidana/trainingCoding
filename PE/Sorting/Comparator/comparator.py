b00d2df747698db8e41ef51374bad540c1b769e6
yasuomaidana <yasuomaidana@gmail.com>
2021-07-31 19:16:14 -0500

    sorting experiment with python

---
+++ PE/Sorting/Comparator/comparator.py
@@ -0,0 +1,20 @@
+from functools import cmp_to_key
+class Player:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+    def __repr__(self):
+        return "{n} {s}".format(n=self.name,s=self.score)
+    def comparator(a, b):
+        pass
+n = int(input())
+data = []
+for i in range(n):
+    name, score = input().split()
+    score = int(score)
+    player = Player(name, score)
+    data.append(player)
+print(data)   
+#data = sorted(data, key=cmp_to_key(Player.comparator))
+#for i in data:
+#    print(i.name, i.score)
