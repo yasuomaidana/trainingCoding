a9aa27aa5ecf6e679adaf3e68e3933b07d35d346
yasuomaidana <yasuomaidana@gmail.com>
2021-08-06 16:30:59 -0500

    Hard Solution

--- JE/Sorting/Mark and Toys/Solution.java
+++ JE/Sorting/Mark and Toys/Solution.java
@@ -26,30 +26,46 @@ class Result {
     private HashMap<Integer,ArrayList<ArrayList<Integer>>> ToysList = new HashMap<>();
     private HashSet<Integer> UsedToys = new HashSet<>();
     private ArrayList<Integer> Reminders = new ArrayList<>();
+    private HashSet<Integer> UsedReminders = new HashSet<>();
     Result(int maximumPrice){
         this.maximumPrice = maximumPrice;
     }
     private void newList(int toy,int reminder){
-        ArrayList<ArrayList<Integer>> currentList;
+        ArrayList<ArrayList<Integer>> currentList = new ArrayList<>();
         ArrayList<Integer> returnList =new ArrayList<>();
         returnList.add(toy);
-        if(ToysList.containsKey(toy)) currentList = ToysList.get(reminder);
-        else currentList = new ArrayList<>();
+        if(ToysList.containsKey(reminder)) 
+        currentList = ToysList.get(reminder);
         currentList.add(returnList);
-        ToysList.put(toy, currentList);
+        ToysList.put(reminder, currentList);
+    }
+    private ArrayList<ArrayList<Integer>> copyLists(int Key){
+        ArrayList<ArrayList<Integer>> rArrayList = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> baseList = ToysList.get(Key);
+        for(int i=0;i<baseList.size();i++){
+            ArrayList<Integer> Line = new ArrayList<>();
+            ArrayList<Integer> baseLine = baseList.get(i);
+            for(int j=0;j<baseLine.size();j++){
+                Line.add(baseLine.get(j));
+            }
+            rArrayList.add(Line);
+        }
+        return rArrayList;
     }
     interface searchStrategy{
         int strategy(int toEvaluate,int element);
     }
     private void addReminder(int reminder){
-        if(Reminders.isEmpty()){ Reminders.add(reminder); return;}
+        if(Reminders.isEmpty()){ Reminders.add(reminder);UsedReminders.add(reminder); return;}
         searchStrategy strategy =(int price,int element)->{
             if (price == element) return 0;
             if(price>element) return 1;
             return -1;
         };
+        if(UsedReminders.contains(reminder)) return;
         int index = findIndex(reminder, strategy)[1];
         Reminders.add(index, reminder);
+        UsedReminders.add(reminder);
     }
     private int[] findIndex(int toEvaluate,searchStrategy strategy){
         int[] minmaxIndex = new int[2];
@@ -83,12 +99,44 @@ class Result {
         }
         minmaxIndex[0]=min;minmaxIndex[1]=max;return minmaxIndex;
     }
-
+    private void calculateMaximumToys(int priceToy){
+        searchStrategy strategy =(int price,int element)->{
+            int reminder = element-price;
+            if (reminder==0) return 0;
+            if(reminder>0) return -1;
+            return 1;
+        };
+        if(priceToy> Reminders.get(Reminders.size()-1)) return;
+        int[] index = findIndex(priceToy, strategy);
+        int start;
+        if(strategy.strategy(priceToy,Reminders.get(index[0]))==1) start = index[1];
+        else start = index[0];
+        ArrayList<Integer> newReminders = new ArrayList<>();
+        int RemSize = Reminders.size();
+        //For each reminder
+        for(int i = start;i<RemSize;i++){
+            int reminder = Reminders.get(i);
+            int destReminder = reminder-priceToy;
+            ArrayList<ArrayList<Integer>> newLists = copyLists(reminder);
+            //For each array of that reminder
+            newLists.forEach(list->{
+                list.add(priceToy);
+                if(list.size()>maximumToys) maximumToys=list.size();
+            });
+            if(ToysList.containsKey(destReminder)){
+                ArrayList<ArrayList<Integer>> rep = ToysList.get(destReminder);
+                rep.addAll(newLists);
+                ToysList.put(destReminder,rep);
+            }else ToysList.put(destReminder,newLists);
+            if(!UsedReminders.contains(destReminder)) newReminders.add(destReminder);
+        }
+        newReminders.forEach(nreminder->addReminder(nreminder));
+    }
     public void addToy(int priceToy){
         int reminder = maximumPrice - priceToy;
         if(reminder<0||UsedToys.contains(priceToy)) return;
         if(ToysList.isEmpty()) maximumToys=1;
-        
+        if(Reminders.size()>0) calculateMaximumToys(priceToy);
         addReminder(reminder);
         newList(priceToy,reminder);
         UsedToys.add(priceToy);
@@ -99,7 +147,7 @@ class Result {
             workingObject.addToy(price);
             
         }
-        return 0;
+        return workingObject.maximumToys;
     }
 
 }
